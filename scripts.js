108cd85bc50aac50479120aec74a165451e5b4b1
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2023-10-18 13:26:06 -0700

    Answered Q2

--- scripts.js
+++ scripts.js
@@ -10,6 +10,17 @@ if (q1test % 3 === 0 && q1test % 5 === 0) {
 
 //Q2. Using an if-else statement, determin if a year is a leap year or not.
 
+let year = 1900;
+// let year = 2000;
+// let year = 1900;
+// let year = 2024;
+
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+  console.log(`Sweet, dude! The year ${year} is a leap year.`);
+  } else {
+    console.log(`The year ${year} is not a leap year.`);
+}
+
 //Q3. Use a loop to print all the numbers from 1 to 100 that are divisible by 3 or 4.
 
 for (i = 1; i <= 100; i++) {
