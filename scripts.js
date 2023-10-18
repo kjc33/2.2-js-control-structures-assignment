c8040d04431b9ea218cacca9042d96a5cc221794
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2023-10-18 01:28:36 -0700

    Updated scripts with comments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Q1. Using an if-else statement, determine if a number is divisible by 3, by 5, or both.
+
 let q1test = 30;
 
 if (q1test % 3 === 0 && q1test % 5 === 0) {
@@ -5,3 +7,7 @@ if (q1test % 3 === 0 && q1test % 5 === 0) {
 } else {
   console.log(`The number ${q1test} is not divisible by three or five.`);
 }
+
+//Q2. Using an if-else statement, determin if a year is a leap year or not.
+
+//Q3. Use a loop to print all the numbers from 1 to 100 that are divisible by 3 or 4.
