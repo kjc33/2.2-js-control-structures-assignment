dba2a9bf7a69fb0e45513ca82ca4cfc3816cf7e8
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2023-10-18 01:13:38 -0700

    Answered Q1

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let q1test = 30;
+
+if (q1test % 3 === 0 && q1test % 5 === 0) {
+  console.log(`The number ${q1test} is divisible by three and five.`);
+} else {
+  console.log(`The number ${q1test} is not divisible by three or five.`);
+}
